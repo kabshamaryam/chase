421e6705ca6ab9a3b08f70b4bf47ef755591b9b5
Kabsha Maryam <kabshamariyam786@gmail.com>
2021-11-23 11:12:31 +0500

    Deployed Project

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React,{Component} from "react";
 import SearchBox from '../components/SearchBox';
 import CardList from "../components/CardList";
 import Scroll from '../components/Scroll';
+import ErrorBoundry from "../components/ErrorBoundry";
 import './App.css';
 
 class App extends Component{
@@ -34,9 +35,11 @@ class App extends Component{
                 <div className='tc'> 
                     <h1 className='f2'>Welcome to the Robo Family</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
-                    <Scroll>
+                    <ErrorBoundry>
+                        <Scroll>
                     <CardList robots={filteredRobots}/>
-                    </Scroll> 
+                        </Scroll> 
+                    </ErrorBoundry>    
                 </div>
             );  
     }           
